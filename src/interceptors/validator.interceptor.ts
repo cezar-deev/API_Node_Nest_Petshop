c18c55175e02992d9352606c8debf57c3c416e0c
Cezar Cardoso <cezar.deev@gmail.com>
2022-08-13 19:11:34 -0300

    Atualizado dados feitos

---
+++ src/interceptors/validator.interceptor.ts
@@ -0,0 +1,34 @@
+import { CallHandler, ExecutionContext, HttpException, HttpStatus, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { Contract } from "src/backpffice/contracts/contract";
+import { Result } from "src/backpffice/models/result.model";
+
+@Injectable()
+export class ValidatorInterceptor implements NestInterceptor {
+constructor(public contract : Contract){
+
+    } // intercept implementado pelo vscode, sem ele da erro nos codigos
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        throw new Error("Method not implemented.");
+    }
+     // intercep criado no curso
+    Intercept(context: ExecutionContext, call$: Observable<any>): 
+    | Observable<any> {
+        const body = context.switchToHttp().getRequest().body
+        const valid = this.contract.validate(body);
+
+        if(!valid){
+            throw new HttpException(
+                new Result(
+                    'Ops, algo saiu errado',
+                    false,
+                    null,
+                    this.contract.errors)
+                    , HttpStatus.BAD_REQUEST); // HttpStatus: existem 3 tipos principais :200=ok/201=criado/400=envio errado do    usuario /401=nao autenticado/ 403=nao autorizado
+                    
+        }
+
+        return call$;
+        
+    }
+}
